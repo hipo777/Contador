5d54755985cb52ab93d3d2a31094dd1ead559cfe
hipolito <huaylla7@hotmail.com>
2023-06-19 21:04:40 -0400

    Boton 1 funcionando

---
+++ app/src/main/java/org/matematicasfacil/contador/MainActivity.java
@@ -0,0 +1,29 @@
+package org.matematicasfacil.contador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMostrar = findViewById(R.id.button1);
+        Button btnContar = findViewById(R.id.button2);
+        TextView textView = findViewById(R.id.textView);
+
+        btnMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Bienvenido al contador", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
